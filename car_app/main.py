97889120356b6cadaa225e0afbf006eea863b872
kangleng <kangleng@gmail.com>
2021-01-14 13:12:12 +0800

    Add car to cars list

--- car_app/main.py
+++ car_app/main.py
@@ -5,13 +5,19 @@ def main():
     car1 = Car("SFF1033J", 1600)
     car2 = Car("ABC999P", 2000)
     car3 = Car("SHA1234X", 1800)
-    car_array = [car1, car2, car3]
-    print_cars(car_array)
+    cars = [car1, car2, car3]
+    car4 = Car("XYZ0000B", 1300)
+    add_car(cars, car4)
+    print_cars(cars)
 
 
 def print_cars(cars):
     for car in cars:
         print(car)
+
+
+def add_car(cars, car):
+    cars.append(car)
 
 
 if __name__ == '__main__':
