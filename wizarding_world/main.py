dba75739f15314d3a4c852b2f2f491b58567ac1e
kangleng <kangleng@gmail.com>
2021-01-15 10:06:30 +0800

    Refactor variable and parameter names

--- wizarding_world/main.py
+++ wizarding_world/main.py
@@ -3,11 +3,15 @@ from wizard import Wizard
 def main():
     wizard1 = Wizard("Harry Potter", 12, 20, 1000)
     wizard2 = Wizard("Dumbledore", 75, 98, 200000)
-    w3 = Wizard("Voldermort", 50, 99, 250000)
+    wizard3 = Wizard("Voldermort", 50, 99, 250000)
 
     print(wizard1)
     print(wizard2)
-    print(w3)
+    print(wizard3)
 
 if __name__ == '__main__':
     main()
+
+# identifiers - 1) meaningful names, 2) avoid short names, 3) follow conventions
+# variables will in lower case and separated by _ for each word
+# function names will be same
--- wizarding_world/wizard.py
+++ wizarding_world/wizard.py
@@ -1,24 +1,24 @@
 class Wizard:
     """Represent a Wizard object"""
 
-    def __init__(self, name, age, level, magPt):
+    def __init__(self, name, age, level, magical_points):
         """Initialize the Wizard object"""
         self.name = name
         self.age = age
         self.level = level
-        self.magPt = magPt
+        self.magical_points = magical_points
 
-    def cast(self, spellPoints):
+    def cast(self, spell_points):
         """Cast spells depending on the magical points"""
-        if self.magPt <= spellPoints:  # False if not enough magical points to cast
+        if self.magical_points <= spell_points:  # False if not enough magical points to cast
             return False
         else:
-            self.magPt -= spellPoints  # Update magical Points when spell is cast
+            self.magical_points -= spell_points  # Update magical Points when spell is cast
             return True
 
     def __str__(self):
         """Return a String Representation of the Wizard object"""
         message = "The Wizard's Name is {}, whose age is {} years. " \
                   "Current level of the Wizard is {}. " \
-                  "Woah {} magical Points!".format(self.name, self.age, self.level, self.magPt)
+                  "Woah {} magical Points!".format(self.name, self.age, self.level, self.magical_points)
         return message
