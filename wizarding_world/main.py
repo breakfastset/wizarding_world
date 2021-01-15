7a4cf67906e9a8f9005736b23665d6846feac0c8
kangleng <kangleng@gmail.com>
2021-01-15 10:08:15 +0800

    Reformat code

--- wizarding_world/main.py
+++ wizarding_world/main.py
@@ -1,5 +1,6 @@
 from wizard import Wizard
 
+
 def main():
     wizard1 = Wizard("Harry Potter", 12, 20, 1000)
     wizard2 = Wizard("Dumbledore", 75, 98, 200000)
@@ -9,9 +10,6 @@ def main():
     print(wizard2)
     print(wizard3)
 
+
 if __name__ == '__main__':
     main()
-
-# identifiers - 1) meaningful names, 2) avoid short names, 3) follow conventions
-# variables will in lower case and separated by _ for each word
-# function names will be same
